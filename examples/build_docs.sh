9810696635ac2de79cdc75af8949cfef9936311b
Jon Brumfitt <jonathan.brumfitt@gmail.com>
2025-02-11 16:42:48 +0000

    Prototype HTML documentation

--- examples/build_docs.sh
+++ examples/build_docs.sh
@@ -4,24 +4,24 @@
 
 if [[ "$PWD" != */tinyqsim ]]
 then
-    echo "Script mist be run from tinyqsim directory"
+    echo "Script must be run from tinyqsim directory"
     exit
 fi						       
 
 cd examples
 
-export OUTH=../doc/generated/
-export OUTM=../doc/generated/
+export OUTH=../doc/html/
+export OUTM=../release/generated/
 
 # Generate HTML versions of notebooks
-#mkdir -p $OUTH
-#jupyter nbconvert --output-dir=$OUTH --to html tutorial_1_getting_started.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html tutorial_2_exploring_further.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html tutorial_3_guide_to_gates.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html tutorial_4_low_level_api.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html example_2_QFT4.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html example_3_quantum_fourier_transform.ipynb
-#jupyter nbconvert --output-dir=$OUTH --to html example_4_quantum_phase_estimation.ipynb
+# mkdir -p $OUTH
+# jupyter nbconvert --output-dir=$OUTH --to html tutorial_1_getting_started.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html tutorial_2_exploring_further.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html tutorial_3_guide_to_gates.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html tutorial_4_low_level_api.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html example_2_QFT4.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html example_3_quantum_fourier_transform.ipynb
+# jupyter nbconvert --output-dir=$OUTH --to html example_4_quantum_phase_estimation.ipynb
 
 # Generate Markdown versions of notebooks
 mkdir -p $OUTM
